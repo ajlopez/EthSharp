0aa1254dc2e898a505b1a1d86c8f00fc57982d6e
ajlopez <webmaster@ajlopez.com>
2016-02-28 08:47:19 -0300

    First DataWord test

---
+++ Src/EthSharp.Tests/Vm/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿namespace EthSharp.Tests.Vm
+{
+    using System;
+    using EthSharp.Vm;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CreateDataWordUsingPositiveInteger()
+        {
+            var dw = new DataWord(1);
+
+            var result = dw.Data;
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(32, result.Length);
+
+            for (int k = 0; k < 31; k++)
+                Assert.AreEqual(0x00, result[k]);
+
+            Assert.AreEqual(0x01, result[31]);
+        }
+    }
+}
---
+++ Src/EthSharp/Vm/DataWord.cs
@@ -0,0 +1,26 @@
+﻿namespace EthSharp.Vm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DataWord
+    {
+        private byte[] data;
+
+        public DataWord(int num)
+        {
+            this.data = new byte[32];
+            int value = num;
+
+            for (int k = 31; k >= 28; k--)
+            {
+                this.data[k] = (byte)(value & 0x00ff);
+                value >>= 8;
+            }
+        }
+
+        public byte[] Data { get { return this.data; } }
+    }
+}
