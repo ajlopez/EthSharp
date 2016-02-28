bcd77437f4216e635b0bdc414a1fad1d64517176
ajlopez <webmaster@ajlopez.com>
2016-02-28 10:19:42 -0300

    RLP encode single byte first test

---
+++ Src/EthSharp.Tests/Util/RlpTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EthSharp.Util;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EthSharp.Tests.Util
+{
+    [TestClass]
+    public class RlpTest
+    {
+        [TestMethod]
+        public void EncodeSingleByteAsOneByte()
+        {
+            var result = Rlp.EncodeByte(0x01);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.Length);
+            Assert.AreEqual(0x01, result[0]);
+        }
+    }
+}
---
+++ Src/EthSharp/Util/Rlp.cs
@@ -0,0 +1,15 @@
+﻿namespace EthSharp.Util
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Rlp
+    {
+        public static byte[] EncodeByte(byte singleByte) 
+        {
+            return new byte[] { singleByte };
+        }
+    }
+}
