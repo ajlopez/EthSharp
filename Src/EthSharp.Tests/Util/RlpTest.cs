d2d210dd1a1f5907c4817ea6a43bf27678403732
ajlopez <webmaster@ajlopez.com>
2016-02-29 05:36:22 -0300

    Fixing source analysis

--- Src/EthSharp.Tests/Util/RlpTest.cs
+++ Src/EthSharp.Tests/Util/RlpTest.cs
@@ -1,9 +1,9 @@
-﻿using System;
-using EthSharp.Util;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace EthSharp.Tests.Util
+﻿namespace EthSharp.Tests.Util
 {
+    using System;
+    using EthSharp.Util;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
     [TestClass]
     public class RlpTest
     {
