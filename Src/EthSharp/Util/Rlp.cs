98b90b241c58f35bdb1a2cd3fbe2e7d4d82fcddd
ajlopez <webmaster@ajlopez.com>
2016-03-07 05:45:50 -0300

    RLP rename constant

--- Src/EthSharp/Util/Rlp.cs
+++ Src/EthSharp/Util/Rlp.cs
@@ -7,23 +7,23 @@
 
     public class Rlp
     {
-        private static readonly int OFFSET_SHORT_ITEM = 0x80;
+        private static readonly int OffsetShortItem = 0x80;
 
         public static byte[] EncodeByte(byte singleByte) 
         {
             if ((singleByte & 0xff) == 0)
-                return new byte[] { (byte)OFFSET_SHORT_ITEM };
+                return new byte[] { (byte)OffsetShortItem };
 
             if ((singleByte & 0xff) <= 0x7f)
                 return new byte[] { singleByte };
 
-            return new byte[] { (byte)(OFFSET_SHORT_ITEM + 1), singleByte };
+            return new byte[] { (byte)(OffsetShortItem + 1), singleByte };
         }
 
         public static byte[] EncodeBytes(byte[] bytes)
         {
             if (bytes == null || bytes.Length == 0)
-                return new byte[] { (byte)OFFSET_SHORT_ITEM };
+                return new byte[] { (byte)OffsetShortItem };
 
             return bytes;
         }
