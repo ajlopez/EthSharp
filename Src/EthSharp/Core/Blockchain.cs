2e3bb8ed85c29798372424f6dad6feb0529447be
ajlopez <webmaster@ajlopez.com>
2016-03-14 06:09:32 -0300

    Blockchain with best block number

---
+++ Src/EthSharp/Core/Blockchain.cs
@@ -0,0 +1,19 @@
+﻿namespace EthSharp.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Blockchain
+    {
+        private Block block;
+
+        public Blockchain(Block block)
+        {
+            this.block = block;
+        }
+
+        public long BestBlockNumber { get { return this.block.Number; } }
+    }
+}
